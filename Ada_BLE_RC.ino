d3162ff5b602756921e8603e7ed49d80ad50bf39
John Edgar Park <jp@jpixl.net>
2017-01-30 20:37:26 -0800

    Update Ada_BLE_RC.ino
    Set to use MotorShield M1 & M2 terminals, commented out while (!Serial); for untethered use.

--- Ada_BLE_RC.ino
+++ Ada_BLE_RC.ino
@@ -38,9 +38,9 @@
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-// And connect 2 DC motors to port M3 & M4 !
-Adafruit_DCMotor *L_MOTOR = AFMS.getMotor(4);
-Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(3);
+// And connect 2 DC motors to port M1 & M2 !
+Adafruit_DCMotor *L_MOTOR = AFMS.getMotor(1);
+Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(2);
 
 //not used, testing acceleration
 // int accelTime = 200;
@@ -73,7 +73,7 @@ char buf[60];
 /**************************************************************************/
 void setup(void)
 {
-  while (!Serial);
+  //while (!Serial);// For use only while plugged into USB an with Serial Monitor open
   AFMS.begin();  // create with the default frequency 1.6KHz
 
   // turn off both motors
